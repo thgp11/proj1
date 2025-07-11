6dfe471f7ea681276bc5c6362404ecb19baa044a
thgp <yyyish99@gmail.com>
2025-07-11 20:08:25 +0900

    Initial commit

---
+++ src/main/java/com/hospital/service/DoctorRegisterService.java
@@ -0,0 +1,8 @@
+package com.hospital.service;
+
+import com.hospital.dto.DoctorRegisterDTO;
+import com.hospital.entity.Doctor;
+
+public class DoctorRegisterService {
+    public Doctor registerDoctor(DoctorRegisterDTO dto, String memberEmail)
+}
